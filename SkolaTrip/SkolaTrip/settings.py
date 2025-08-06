eaa82825fcb4d055ee792f4d81faa5a46b2f131f
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-06 13:43:28 +0000

    Aug 6, 2025, 5:43 PM

--- SkolaTrip/SkolaTrip/settings.py
+++ SkolaTrip/SkolaTrip/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'SkolaTrip.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': "skolatrip",
+        'USER': 'skolatrip',
+        'PASSWORD': '',
+        'HOST': 'dpg-d29ln3ali9vc73fstssg-a',
+        'PORT': '5432',
     }
 }
 
