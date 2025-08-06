530b70cdc9688befeec5bc8aa4905bd5b24416d5
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-06 15:14:36 +0000

    Aug 6, 2025, 7:14 PM

--- SkolaTrip/st/models.py
+++ SkolaTrip/st/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     full_name = models.CharField(max_length=255, blank=True, null=True)
-    
+    phone_number = models.CharField(max_length=100, blank=True, null=True)
