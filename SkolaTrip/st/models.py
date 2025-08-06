be0840bd917ca7726ecaad619f936979dbf1491e
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-06 15:19:44 +0000

    Aug 6, 2025, 7:19 PM

--- SkolaTrip/st/models.py
+++ SkolaTrip/st/models.py
@@ -1,9 +1,24 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class User(AbstractUser):
     full_name = models.CharField(max_length=255, blank=True, null=True)
     phone_number = models.CharField(max_length=100, blank=True, null=True)
     class_name = models.CharField(max_length=255, blank=True, null=True)
-    student_count = models.PositiveIntegerField(blank=True, null=True)
+    student_count = models.PositiveIntegerField(default=0)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_groups",  # avoids clash
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions",  # avoids clash
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
