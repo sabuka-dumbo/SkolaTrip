6327d1586b8b04c830955b655ef367da93842c51
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-06 15:17:23 +0000

    Aug 6, 2025, 7:17 PM

--- SkolaTrip/st/models.py
+++ SkolaTrip/st/models.py
@@ -5,3 +5,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     full_name = models.CharField(max_length=255, blank=True, null=True)
     phone_number = models.CharField(max_length=100, blank=True, null=True)
+    class_name = models.CharField(max_length=255, blank=True, null=True)
+    student_count = models.PositiveIntegerField(blank=True, null=True)
