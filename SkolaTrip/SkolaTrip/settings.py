2568ae779e6732cec66cfb71e167c2d926ad4054
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-07 05:47:01 +0000

    Aug 7, 2025, 9:47 AM

--- SkolaTrip/SkolaTrip/settings.py
+++ SkolaTrip/SkolaTrip/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SkolaTrip.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'RHlQzsSxgCgCrZZfljIXqreNpCegdiag',
+        'HOST': 'postgresql://postgres:RHlQzsSxgCgCrZZfljIXqreNpCegdiag@gondola.proxy.rlwy.net:26323/railway',
+        'PORT': '5432',
     }
 }
 
